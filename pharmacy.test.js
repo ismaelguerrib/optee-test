cda66995a6e5588fdc804d7dc44a85677e696165
Ismaël <ismael.guerrib@hotmail.fr>
2024-07-14 20:48:35 +0200

    test(updateBenefitValue): add tests for general rules applicable to all drugs
    Add unit tests to ensure that all drugs adhere to the general rules:
    - Benefit and ExpiresIn decreases each day
    - Benefit does not exceed 50
    - Benefit never drops below 0
    - Benefit decrease twice as fast when drug is expired

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -6,4 +6,19 @@ describe("Pharmacy", () => {
       new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()
     ).toEqual([new Drug("test", 1, 2)]);
   });
+  it("should decrease the benefit twice as fast when expired", () => {
+    expect(
+      new Pharmacy([new Drug("test", 0, 3)]).updateBenefitValue()
+    ).toEqual([new Drug("test", -1, 1)]);
+  });
+  it("should not decrease the benefit below 0", () => {
+    expect(
+      new Pharmacy([new Drug("test", 0, 0)]).updateBenefitValue()
+    ).toEqual([new Drug("test", -1, 0)]);
+  });
+  it("should not increase the benefit above 50", () => {
+    expect(
+      new Pharmacy([new Drug("Herbal Tea", 2, 50)]).updateBenefitValue()
+    ).toEqual([new Drug("Herbal Tea", 1, 50)]);
+  });
 });
